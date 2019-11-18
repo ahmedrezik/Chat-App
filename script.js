e8fc7874b77def5eddf1eed77e991a6e078849d6
Ahmed Abdulkareem Rezik <ahmedrezq100@hotmail.com>
2019-11-18 06:09:58 +0100

    intial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const socket = io('http://localhost:3000')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+const messageContainer = document.getElementById('message-container')
+
+
+
+
+const name  = prompt("What's your name?")
+appendMessage("Welcome to the chat" + "    " + name)
+
+socket.emit('new-user',name)
+socket.on('user-connected', name => {
+	appendMessage(name + '  ' + 'connected')
+})
+
+socket.on('chat-message', data => {
+
+
+	appendMessage(data.name + ": " + data.message)
+})
+
+socket.on('user-disconnected', name => {
+  appendMessage(name + '  disconnected')
+})
+
+messageForm.addEventListener('submit', e =>{
+
+
+	e.preventDefault()
+	const message = messageInput.value
+	appendMessage('You: ' + message)
+	socket.emit('send-chat-message',message)
+	messageInput.value = ""
+})
+
+function appendMessage(message){
+
+const messageElement = document.createElement('div')
+messageElement.innerText = message
+messageContainer.append(messageElement)
+
+}
